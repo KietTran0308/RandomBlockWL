d8f9706dd2768540b4a60b58cd5731f6b6755d3b
KietTran0308 <51715024+KietTran0308@users.noreply.github.com>
2020-02-03 16:03:19 +0700

    Add files via upload

---
+++ src/WL/Main.php
@@ -0,0 +1,145 @@
+<?php
+
+
+
+namespace WL;
+
+
+
+use pocketmine\plugin\PluginBase;
+
+use pocketmine\Server;
+
+use pocketmine\event\Listener;
+
+use pocketmine\command\{Command, CommandSender, ConsoleCommandSender, CommandExecutor};
+
+use pocketmine\Player;
+
+
+
+use pocketmine\event\block\BlockUpdateEvent;
+
+
+
+use pocketmine\item\Item;
+
+use pocketmine\level\Level;
+
+use pocketmine\block\{Block, Cobblestone, Water, Lava, Iron, Diamond, Emerald, Gold, Coal, Lapis, Redstone, Stone};
+
+
+
+class Main extends PluginBase implements Listener{
+
+    
+
+    public function onEnable(): void{
+
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+
+        $this->getLogger()->info("§c§l\|/ §aPLUGIN Random Block Water Với Lava Đã Bật \|/");
+
+    }
+
+    
+
+    public function block(BlockUpdateEvent $event){
+
+        $block = $event->getBlock();
+
+        $water = false;
+
+        $lava = false;
+
+        for ($i = 2; $i <= 5; $i++) {
+
+            $nearBlock = $block->getSide($i);
+
+            if ($nearBlock instanceof Water) {
+
+                $water = true;
+
+            } else if ($nearBlock instanceof Lava) {
+
+                $lava = true;
+
+            }
+
+            if ($water && $lava) {
+
+                $id = mt_rand(1, 20);
+
+                switch ($id) {
+
+                    case 2;
+
+                        $newBlock = new Cobblestone();
+
+                        break;
+
+                    case 4;
+
+                        $newBlock = new Iron();
+
+                        break;
+
+                    case 6;
+
+                        $newBlock = new Gold();
+
+                        break;
+
+                    case 8;
+
+                        $newBlock = new Emerald();
+
+                        break;
+
+                    case 10;
+
+                        $newBlock = new Coal();
+
+                        break;
+
+                    case 12;
+
+                        $newBlock = new Redstone();
+
+                        break;
+
+                    case 14;
+
+                        $newBlock = new Lapis();
+
+                        break;
+
+                    case 16;
+
+                        $newBlock = new Diamond();
+
+                        break;
+
+                    default:
+
+                        $newBlock = new Cobblestone();
+
+                        $newBlock = new Cobblestone();
+
+                        $newBlock = new Stone();
+
+                        $newBlock = new Stone();
+
+                }
+
+                $block->getLevel()->setBlock($block, $newBlock, true, false);
+
+                return;
+
+            }
+
+        }
+
+    }
+
+}
